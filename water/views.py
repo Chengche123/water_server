4c3eea582e0c85737522db4c5a2072e7d60f0bd6
佘曾伟 <chengche245@163.com>
2022-04-08 19:21:06 +0800

    feat(api): 添加 GET /users/login 登录接口

--- water/views.py
+++ water/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework import authentication
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
@@ -38,6 +40,24 @@ class HX2021ViewSet(viewsets.ReadOnlyModelViewSet):
     authentication_classes = [authentication.SessionAuthentication, MyBasicAuthentication]
     # 权限
     permission_classes = [permissions.IsAuthenticated]
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_authenticate_header(self, request):
+        return {}
+
+    @action(detail=False,
+            methods=['GET'],
+            permission_classes=[permissions.IsAuthenticated],
+            authentication_classes=[MyBasicAuthentication])
+    def login(self, request, pk=None):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
 
 @api_view(['GET'])
