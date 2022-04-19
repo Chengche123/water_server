d283a0b75d60fed3cfdd8d46bef07fd7d635f25f
佘曾伟 <chengche245@163.com>
2022-04-19 19:04:46 +0800

    feat(admin): 增加管理页面

--- water/admin.py
+++ water/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from water.models import UserExtend
+
+
+# Define an inline admin descriptor for UserExtend model
+# which acts a bit like a singleton
+class UserExtendInline(admin.StackedInline):
+    model = UserExtend
+    can_delete = False
+    verbose_name_plural = 'user_extend'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserExtendInline,)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- water/urls.py
+++ water/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
@@ -14,4 +15,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('accounts/', include('rest_framework.urls')),
     path('user', views.user),
+    path('admin/', admin.site.urls),
 ]
