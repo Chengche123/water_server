22c19f373980f0b51fe8e10cf544d01a5981ebf1
佘曾伟 <chengche245@163.com>
2022-04-19 19:04:25 +0800

    feat(model): add user extend

--- water/models.py
+++ water/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class USensor(models.Model):
@@ -108,3 +109,11 @@ class HX2022(models.Model):
 
     def __str__(self):
         return str(self.value)
+
+
+class UserExtend(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telephone_number = models.CharField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user)
